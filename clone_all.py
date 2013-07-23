f704eb15647418b2442083467c57ad9777fc8c91
Loy van Beek <loy.vanbeek@gmail.com>
2013-07-23 21:40:20 +0200

    Made setup.py. First time ever for me

--- clone_all.py
+++ clone_all.py
@@ -10,29 +10,46 @@ from sh import git, cd, svn, grep
 
 import xml.etree.ElementTree as ET
 
-TOKEN = sys.argv[1]
-
 sourceRoot = "~/ros/fuerte/tue/trunk"
 destinationRoot = "~/ros/fuerte/tue/git"
 
 sourceRoot = os.path.expanduser(sourceRoot)
 destinationRoot = os.path.expanduser(destinationRoot)
 
-g = Github(login_or_token=TOKEN)
 
-def clone_all_tue_packages():
-	cd(destinationRoot)
 
-	tue = g.get_organization("tue-robotics-lab")
+def clone_all_tue_packages(use_https):
+    cd(destinationRoot)
 
-	tue_repos = tue.get_repos()
+    tue = g.get_organization("tue-robotics-lab")
 
-	for repo in tue_repos:
-		try:
-			git.clone(repo.clone_url)
-		except sh.ErrorReturnCode_128 as e: #When the dir to clone already exists and is not empty, git pull this dir
-			cd(repo.name)
-			git.pull()
+    tue_repos = tue.get_repos()
+
+    for repo in tue_repos:
+        if use_https:
+            git_url = repo.clone_url
+        else:
+            git_url = repo.ssh_url
+
+        try:
+            git.clone(git_url)
+        except sh.ErrorReturnCode_128 as e:  # When the dir to clone already exists and is not empty, git pull this dir
+            cd(repo.name)
+            git.pull("origin", "master")
 
 if __name__ == "__main__":
-	clone_all_tue_packages()
+    TOKEN = sys.argv[1]
+    g = Github(login_or_token=TOKEN)
+
+
+    try:
+        method = sys.argv[2]
+        if method == "--ssh":
+            use_https = False
+        elif method == "--https":
+            use_https = True
+    except IndexError:
+        use_https = True
+
+
+    clone_all_tue_packages(use_https)
